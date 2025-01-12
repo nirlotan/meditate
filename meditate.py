0236fc8081984bfc10f9722ca40330c8e6307475
Nir Lotan <nir.lotan@gmail.com>
2025-01-12 16:04:56 +0200

    update subcategories default

--- meditate.py
+++ meditate.py
@@ -20,7 +20,8 @@ selected_categories = st.segmented_control("Select Category", categories, select
 sub_categories = pd.DataFrame(df[df['category'].isin(selected_categories)]['sub_category'].unique(),columns=['SubCategory'])
 
 if len(sub_categories) > 0:
-    selected_sub_categories = st.segmented_control("Select SubCategory", sub_categories, selection_mode="multi")
+    selected_sub_categories = st.segmented_control("Select SubCategory", sub_categories, selection_mode="multi",
+                                                                        default = sub_categories)
 
 duration_selection = st.slider("Duration", min_value=min_duration, 
               max_value=max_duration, value=(min_duration,max_duration)),
