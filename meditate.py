902a1bf6a37f6e79b646b86d4fb3316104d1772f
Nir Lotan <nir.lotan@gmail.com>
2025-01-12 16:01:07 +0200

    subcategories

--- meditate.py
+++ meditate.py
@@ -14,32 +14,12 @@ min_duration = df['duration'].min()
 max_duration = df['duration'].max()
 
 categories = pd.DataFrame(df['category'].unique(),columns=['Category'])
-"""
-category_selection = st.sidebar.dataframe(categories,
-                                  on_select='rerun',
-                                  hide_index = True,
-                                  selection_mode = "multi-row",
-                                  )
-"""
-selected_categories = st.segmented_control("Category", categories, selection_mode="multi")
-"""
-if len(category_selection.selection['rows']):
-    selected_categories = [categories['Category'].iloc[row] for row in category_selection]
-else:
-    selected_categories = categories['Category'].values
-"""
+selected_categories = st.segmented_control("Select Category", categories, selection_mode="multi")
+
 sub_categories = pd.DataFrame(df[df['category'].isin(selected_categories)]['sub_category'].unique(),columns=['SubCategory'])
 
-subcategory_selection = st.sidebar.dataframe(sub_categories,
-                                  on_select='rerun',
-                                  hide_index = True,
-                                  selection_mode = "multi-row",
-                                  )
-if len(subcategory_selection.selection['rows']):
-    selected_sub_categories = [sub_categories['SubCategory'].iloc[row] for row in subcategory_selection.selection['rows']]
-else:
-    selected_sub_categories = sub_categories['SubCategory'].values
-
+if len(sub_categories) > 0:
+    subcategory_selection = st.segmented_control("Select SubCategory", selected_categories, selection_mode="multi")
 
 duration_selection = st.slider("Duration", min_value=min_duration, 
               max_value=max_duration, value=(min_duration,max_duration)),
